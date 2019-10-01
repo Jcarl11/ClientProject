32761a7979bf7d40c93296c1c3a564edfe3467b3
JOEY CARLO FRANCISCO <jcpfrancisco@mymail.mapua.edu.ph>
2019-10-01 10:34:39 +0800

    Changed properties

---
+++ Calculator/PropertiesClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class PropertiesClass
+    {
+        private double latitude1 = 0;
+        private double longitude1 = 0;
+        private double longitude2 = 0;
+        private double longitude = 0;
+        private double distance = 0;
+        private double azimuth = 0;
+        private double C = 0;
+
+        
+    }
+}
